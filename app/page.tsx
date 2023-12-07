cb70cc18565434c6c29fc2b114863a7d9247c3ab
SAM LAW <sam@mrsamlaw.com>
2023-12-07 13:41:11 +1100

    Initial Commit | Added Geist

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      This is CHONGLAND things.
+
+      Trying to rebuild this following the direction of Lee Robinson's blog refresh with minimal dependancies on external packages etc etc.
+    </div>
+  )
+}
